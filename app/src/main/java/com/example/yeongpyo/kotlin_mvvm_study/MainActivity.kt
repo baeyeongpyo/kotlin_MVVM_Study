ae888064160bf8c968db8b7ca7da946c3c02c32f
yeongpyo <byp93@naver.com>
2018-04-25 04:07:19 +0900

    2018-04-25-04:07 Binding Test

--- app/src/main/java/com/example/yeongpyo/kotlin_mvvm_study/MainActivity.kt
+++ app/src/main/java/com/example/yeongpyo/kotlin_mvvm_study/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.yeongpyo.kotlin_mvvm_study
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yeongpyo.kotlin_mvvm_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        Binding.setDatastring("Test Code")
+
     }
 }
---
+++ app/src/main/java/com/example/yeongpyo/kotlin_mvvm_study/VMData.kt
@@ -0,0 +1,4 @@
+package com.example.yeongpyo.kotlin_mvvm_study
+
+class VMData {
+}
